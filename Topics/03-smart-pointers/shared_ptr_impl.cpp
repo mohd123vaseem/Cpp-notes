03dc4db9224d8efa87865718f3aaf3c8830aa87a
mohd123vaseem <muhammad123vaseem@gmail.com>
2026-07-09 15:39:27 +0530

    Feat : stage 3 impl of shared_ptr : added copy constructor and made ref count private inside the obj.

--- Topics/03-smart-pointers/shared_ptr_impl.cpp
+++ Topics/03-smart-pointers/shared_ptr_impl.cpp
@@ -5,35 +5,47 @@ using namespace std;
 class shared_ptr
 {
 
+    int *ref_cnt ;
+
 public:
     int *ptr;
-    int *ref_cnt;
-    shared_ptr(int *p,int *cnt)
+
+    // normal constructor.
+    shared_ptr(int *p)
     {
         ptr = p;
-        ref_cnt=cnt;
-        *ref_cnt+=1;
+
+        ref_cnt = new int(1);
         cout << "shared_ptr " << *ptr << " has ref count " << *ref_cnt << endl;
+    }
+
+    // copy constructor
+    shared_ptr(const shared_ptr &other)
+    {
+        this->ptr = other.ptr;
+        this->ref_cnt = other.ref_cnt;
+        *(this->ref_cnt) += 1;
+        cout << "COPY shared_ptr " << *(this->ptr) << " has ref count " << *(this->ref_cnt) << endl;
     }
 
     ~shared_ptr()
     {
         if (*ref_cnt != 1)
         {
-            cout << "shared_ptr " << *ptr << " has ref count " << *ref_cnt << " ,so NO DELETE"<<endl;
-            *ref_cnt-=1;
+            cout << "shared_ptr " << *ptr << " has ref count " << *ref_cnt << " ,so NO DELETE" << endl;
+            *ref_cnt -= 1;
             return;
         }
-        cout<<"\n";
+        cout << "\n";
         cout << "shared_ptr " << *ptr << " has ref count " << *ref_cnt << " ,so we DELETE NOW" << endl;
         delete ptr; // after this heap memo is freed that was occupied by 10.
         // now pointer is dangling ,as it points to add that has no vlaue.
         ptr = nullptr; // now its safe as it does not point to freed memo.
         cout << "ptr is freed along with heap memo" << endl;
         cout << "pointer points to null" << ptr << endl;
-        //did same for refcount.
+        // did same for refcount.
         delete ref_cnt;
-        ref_cnt=nullptr;
+        ref_cnt = nullptr;
     }
 };
 
@@ -42,21 +54,20 @@ int main()
 
     cout << "hello world" << endl;
     int *temp_ptr1 = new int(10);
-    int *cnt1 = new int(0);
     int *temp_ptr2 = new int(123);
-    int *cnt2 = new int(0);
-    shared_ptr p1 = shared_ptr(temp_ptr1,cnt1);
-    shared_ptr p2 = shared_ptr(temp_ptr1,cnt1);
-    shared_ptr p3 = shared_ptr(temp_ptr1,cnt1);
-    shared_ptr p4 = shared_ptr(temp_ptr2,cnt2);
-    shared_ptr p5 = shared_ptr(temp_ptr2,cnt2);
+
+    shared_ptr p1 = shared_ptr(temp_ptr1);
+    shared_ptr p2 = p1;
+    shared_ptr p3 = p1;
+    shared_ptr p4 = shared_ptr(temp_ptr2);
+    shared_ptr p5 = p4;
 
     cout << "value of pointer is " << *(p1.ptr) << " ,Address of value is " << (p1.ptr) << endl;
     cout << "value of pointer is " << *(p2.ptr) << " ,Address of value is " << (p2.ptr) << endl;
     cout << "value of pointer is " << *(p3.ptr) << " ,Address of value is " << (p3.ptr) << endl;
-    cout<<"\n";
+    cout << "\n";
     cout << "value of pointer is " << *(p4.ptr) << " ,Address of value is " << (p4.ptr) << endl;
     cout << "value of pointer is " << *(p5.ptr) << " ,Address of value is " << (p5.ptr) << endl;
-    cout<<"\n";
+    cout << "\n";
     return 0;
 }
