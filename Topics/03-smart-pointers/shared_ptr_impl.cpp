3c2c334bab1b542d84d7c6580644bbcf1bc46811
mohd123vaseem <muhammad123vaseem@gmail.com>
2026-07-11 23:48:39 +0530

    Feat : shared ptr stage 4 done.

--- Topics/03-smart-pointers/shared_ptr_impl.cpp
+++ Topics/03-smart-pointers/shared_ptr_impl.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class shared_ptr
 {
 
-    int *ref_cnt ;
+    int *ref_cnt;
 
 public:
     int *ptr;
@@ -26,6 +26,33 @@ public:
         this->ref_cnt = other.ref_cnt;
         *(this->ref_cnt) += 1;
         cout << "COPY shared_ptr " << *(this->ptr) << " has ref count " << *(this->ref_cnt) << endl;
+    }
+
+    // Assignment constructor
+    shared_ptr &operator=(const shared_ptr &other)
+    {
+
+        if (this == &other)
+            return *this; // bail early on self assignment.
+
+        if (*this->ref_cnt == 1)
+        {   
+            cout<<"DELETE BEFORE ASSIGNMENT  "<<this->ptr<<" ,has value "<<*this->ptr<<" ,and ref_count as "<<*this->ref_cnt<<endl;
+            delete this->ptr;
+            delete this->ref_cnt;
+        }
+        else
+        {    
+            cout<<"DECREMENT REF_COUNT BEFORE ASSIGNMENT "<<this->ptr<<" ,has value "<<*this->ptr<<" ,and ref_count as "<<*this->ref_cnt<<endl;
+            *this->ref_cnt -= 1;
+        }
+
+        this->ptr = other.ptr;
+        this->ref_cnt = other.ref_cnt;
+        *this->ref_cnt += 1;
+        cout<<"ASSIGNMENT done "<<this->ptr<<" ,has value "<<*this->ptr<<" ,and ref_count as "<<*this->ref_cnt<<endl;
+
+        return *this;
     }
 
     ~shared_ptr()
@@ -57,17 +84,17 @@ int main()
     int *temp_ptr2 = new int(123);
 
     shared_ptr p1 = shared_ptr(temp_ptr1);
-    shared_ptr p2 = p1;
-    shared_ptr p3 = p1;
+    shared_ptr p2 = p1;//coping the p1 ptr
+    // shared_ptr p3 = p1;
     shared_ptr p4 = shared_ptr(temp_ptr2);
-    shared_ptr p5 = p4;
+    p4 = p1;//assigning the ptr p1 to p4.
 
-    cout << "value of pointer is " << *(p1.ptr) << " ,Address of value is " << (p1.ptr) << endl;
+    cout << "value of pointer is " << *(p1.ptr) << " ,Address of value is " << (p1.ptr)<< endl;
     cout << "value of pointer is " << *(p2.ptr) << " ,Address of value is " << (p2.ptr) << endl;
-    cout << "value of pointer is " << *(p3.ptr) << " ,Address of value is " << (p3.ptr) << endl;
+    // cout << "value of pointer is " << *(p3.ptr) << " ,Address of value is " << (p3.ptr) << endl;
     cout << "\n";
     cout << "value of pointer is " << *(p4.ptr) << " ,Address of value is " << (p4.ptr) << endl;
-    cout << "value of pointer is " << *(p5.ptr) << " ,Address of value is " << (p5.ptr) << endl;
+    // cout << "value of pointer is " << *(p5.ptr) << " ,Address of value is " << (p5.ptr) << endl;
     cout << "\n";
     return 0;
 }
